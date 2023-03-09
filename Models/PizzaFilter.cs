dc7dbe6ab6800bca7bba0b3ed659b06d26203a34
ast <ast@htc-cs.com>
2023-03-09 16:26:03 +0400

    заготовка кода для классического подхода

--- Models/PizzaFilter.cs
+++ Models/PizzaFilter.cs
@@ -1,4 +1,12 @@
 public class PizzaFilter
 {
-
+    public string Name { get; set; } //Like
+    public string Description { get; set; } //Like
+    public int? KCalMin { get; set; }
+    public int? KCalMax { get; set; }
+    public decimal? PriceMin { get; set; }
+    public decimal? PriceMax { get; set; }
+    public int? DiameterMin { get; set; }
+    public int? DiameterMax { get; set; }
+    public IEnumerable<string> Ingridients { get; set; } //has
 }
--- Services/PizzaSerivce.cs
+++ Services/PizzaSerivce.cs
@@ -1,12 +1,20 @@
 public class PizzaService
 {
-    public Task<IEnumerable<Pizza>> Get()
+    public Task<IEnumerable<Pizza>> Get(PizzaRequest req)
     {
-        //...
+        var q = _db/*.AsNoTracking()*/;
+        
+        if (!string.IsNullOrEmpty(req.Filter.Name))
+            q = q.Where(p => p.Name == req.Filter.Name); //можно Like
+        if (!string.IsNullOrEmpty(req.Filter.Description))
+            q = q.Where(p => p.Description == req.Filter.Description); //можно Like
+            //...
+
+        
         return null;
     }
 
-    public Task<IEnumerable<Pizza>> GetEx()
+    public Task<IEnumerable<Pizza>> GetEx(PizzaRequest req)
     {
         //...
         return null;
@@ -14,9 +22,9 @@ public class PizzaService
 
     #region [ dummy data ]
 
-    IEnumerable<Pizza> Pizzas { get; } = data;
+    IQueryable<Pizza> _db { get => _data.AsQueryable(); }
 
-    static Pizza[] data = new Pizza[]
+    static Pizza[] _data = new Pizza[]
     {
         new Pizza()
         {
